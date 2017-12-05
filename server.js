e5f6e14094003c59b5239ee1969f59dc85f86099
Eli Sandine <sandinee@oregonstate.edu>
2017-12-05 10:35:19 -0800

    server.js is working with node, created package.json file as well

---
+++ server.js
@@ -0,0 +1,58 @@
+/*--------------------creating server------------------------------*/
+var http = require('http'); // require http module to create server
+var fs = require('fs'); // require fs module to read files
+
+// reading all the files in once
+var indexHtml = fs.readFileSync('./public/index.html', 'utf8');
+var styleCSS = fs.readFileSync('./public/style.css', 'utf8');
+var indexJS = fs.readFileSync('./public/index.js', 'utf8');
+var foodMonsterJPG = fs.readFileSync('./public/foodMonster.jpg');
+var pageNotFound = fs.readFileSync('./public/404.html', 'utf8');
+
+// server function with callback
+var server = http.createServer( function (req, res) {
+  console.log("== Request made");
+  console.log("  - method:", req.method);
+  console.log("  - path:", req.url);
+  console.log("  - headers:", req.headers);
+
+  if (req.url === '/' || req.url === '/index.html') {
+    res.writeHead(200, {
+      'Content-Type': 'text/html'  // specifying expected content type
+    });
+    res.write(indexHtml);
+    res.end('\n');
+  }
+  else if (req.url === '/style.css') {
+    res.writeHead(200, {
+      'Content-Type': 'text/css'  // specifying expected content type
+    });
+    res.write(styleCSS);
+    res.end('\n');
+  }
+  else if (req.url === '/index.js') {
+    res.writeHead(200, {
+      'Content-Type': 'text/js'  // specifying expected content type
+    });
+    res.write(indexJS);
+    res.end('\n');
+  }
+  else if (req.url === '/foodMonster.jpg') {
+    res.writeHead(200, {
+      'Content-Type': 'image/jpeg'  // specifying expected content type
+    });
+    res.write(foodMonsterJPG);
+    res.end('\n');
+  }
+  else {    // default case is the 404.html page
+    res.writeHead(404, {
+      'Content-Type': 'text/html' // specifying expected content type
+    });
+    res.write(pageNotFound);
+    res.end('\n');
+  }
+});
+/*---------------------Specifying Port-----------------------------*/
+var port = process.env.PORT || 3000; // If port isn't specified then use 3000 as default.
+
+server.listen(port); 
