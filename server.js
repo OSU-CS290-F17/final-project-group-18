6983cd7fcac76fe5816baac466f9cf35026e4882
Eli Sandine <sandinee@oregonstate.edu>
2017-12-07 13:39:32 -0800

    Made the header link to the home page, removed commented out old server from server.js, changed green color to match the monster better, moved comment box to bottom of modal dialog box, changed hover button colors.

--- server.js
+++ server.js
@@ -1,60 +1,4 @@
 /*--------------------creating server------------------------------*/
-// var http = require('http'); // require http module to create server
-// var fs = require('fs'); // require fs module to read files
-
-// reading all the files in once
-// var indexHtml = fs.readFileSync('./public/index.html', 'utf8');
-// var styleCSS = fs.readFileSync('./public/style.css', 'utf8');
-// var indexJS = fs.readFileSync('./public/index.js', 'utf8');
-// var foodMonsterJPG = fs.readFileSync('./public/foodMonster.jpg');
-// var pageNotFound = fs.readFileSync('./public/404.html', 'utf8');
-
-// server function with callback
-// var server = http.createServer( function (req, res) {
-  /*
-  console.log("== Request made");
-  console.log("  - method:", req.method);
-  console.log("  - path:", req.url);
-  console.log("  - headers:", req.headers);
-  */
-
-//   if (req.url === '/' || req.url === '/index.html') {
-//     res.writeHead(200, {
-//       'Content-Type': 'text/html'  // specifying expected content type
-//     });
-//     res.write(indexHtml);
-//     res.end('\n');
-//   }
-//   else if (req.url === '/style.css') {
-//     res.writeHead(200, {
-//       'Content-Type': 'text/css'  // specifying expected content type
-//     });
-//     res.write(styleCSS);
-//     res.end('\n');
-//   }
-//   else if (req.url === '/index.js') {
-//     res.writeHead(200, {
-//       'Content-Type': 'text/js'  // specifying expected content type
-//     });
-//     res.write(indexJS);
-//     res.end('\n');
-//   }
-//   else if (req.url === '/foodMonster.jpg') {
-//     res.writeHead(200, {
-//       'Content-Type': 'image/jpeg'  // specifying expected content type
-//     });
-//     res.write(foodMonsterJPG);
-//     res.end('\n');
-//   }
-//   else {    // default case is the 404.html page
-//     res.writeHead(404, {
-//       'Content-Type': 'text/html' // specifying expected content type
-//     });
-//     res.write(pageNotFound);
-//     res.end('\n');
-//   }
-// });
-
 var path = require('path');
 var express = require('express');
 var exphbs = require('express-handlebars');
@@ -95,8 +39,6 @@ app.get('/', function (req, res) {
       });
     }
   });
-
-// res.status(200).render('postsPage', )
 });
 
 app.get('/:n', function (req, res) {
@@ -118,7 +60,6 @@ app.get('/:n', function (req, res) {
     }
   });
 });
-
 
 app.get('*', function (req, res) {
   res.status(404).render('404');
@@ -180,13 +121,11 @@ app.delete('/delete/:n', function(req, res) {
       }
     }
   });
-
 });
 
 app.delete('*', function(req, res) {
   res.status(404).render('404');
 });
-
 
 /*---------------------Specifying Port-----------------------------*/
 var port = process.env.PORT || 3000; // If port isn't specified then use 3000 as default.
@@ -200,7 +139,3 @@ MongoClient.connect(mongoURL, function (err, connection) {
     console.log("== Server listening on port:", port);
   });
 });
-
-// server.listen(port, function () {
-//   console.log("== Server listening on port:", port);
-// });
